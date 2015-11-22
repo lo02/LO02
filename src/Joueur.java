3dc2843a3a8af94e520fc2372622e80cf93b7f83
Gilles <Gilles@PC-Gilles>
2015-11-22 20:13:46 +0100

    correction de poserCarte()

--- src/Joueur.java
+++ src/Joueur.java
@@ -3,7 +3,6 @@ import java.util.List;
 import java.util.Random;
 
 public class Joueur {
-	
 
 	protected String nom = "";
 	protected int nbreMenhir = 0;
@@ -16,29 +15,22 @@ public class Joueur {
 	
 	public void remplirMainJoueur()
 	{
-		
-		Card tasDeCarte = new Ingredient();
-		
+		Card tasDeCarte = new Ingredient();		
 		// linked list mettre un queue
 		List<Card> tempTas = new ArrayList<Card>();
-		tempTas.addAll(tasDeCarte.getTasDeCartes());
-		
+		tempTas.addAll(tasDeCarte.getTasDeCartes());	
 		for(int i=0;i<4;i++)
 		{
 			main.add(tempTas.get(0));
 			tempTas.remove(0);
-			
 		}
-		tasDeCarte.setTasDeCartes(tempTas);		
-	
-		
+		tasDeCarte.setTasDeCartes(tempTas);			
 	}
 	
 	public String getNom() {
 		return nom;
 	}
 
-	
 	public Joueur(String nom, int nbreMenhir, int nbreGraine) {
 		this.nom = nom;
 		this.nbreMenhir = nbreMenhir;
@@ -68,7 +60,6 @@ public class Joueur {
 	public List<Card> getMain(){
 		return this.main;
 	}	
-	
 	
 	// Méthode poser carte qui va permettre de jouer Engrais ou Géant
 	public void poserCarte(int index , int action )
@@ -110,15 +101,13 @@ public class Joueur {
 		{
 			if(nbrGrainesCarte > this.nbreGraine)
 			{
-				this.nbreMenhir = this.nbreMenhir + nbrGrainesJoueur;
+				this.nbreGraine = this.nbreGraine + nbrGrainesJoueur;
 				joueurCible.setNbreGraine(0);
 			}
 		}
 		this.main.remove(index);
-		
-				
-		
 	}
+	
 	public void planterGraines(int nbrGrainesCarte){
 		if(nbrGrainesCarte <= this.nbreGraine)
 		{
@@ -131,22 +120,17 @@ public class Joueur {
 				this.nbreMenhir = this.nbreMenhir + this.nbreGraine;
 				this.nbreGraine = 0;
 			}
-		}
-	
-		
+		}	
 	}
 	
-
-
 	public String toString()
 	{
 		return "Joueur " + this.nom +":\n- Nombres ménhirs : "+ this.nbreMenhir +"\n- Nombres graines : "+ this.nbreGraine+"\n\n====================\n\n";
 	
 	}
-	
+
 	public int[] choisirCarte()
-	{
-		
+	{		
 		return null;
 	}
 }
