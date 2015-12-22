7bfd2cefdd84fbf1e2af9496f8287d21594be81a
Gilles <Gilles@PC-Gilles.home>
2015-12-22 17:27:08 +0100

    mouseClicked

--- src/SelectionDeCarte.java
+++ src/SelectionDeCarte.java
@@ -14,6 +14,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SelectionDeCarte {
 
@@ -55,70 +57,70 @@ public class SelectionDeCarte {
 		frame.getContentPane().setLayout(null);
 		
 		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(12, 90, 982, 335);
+		layeredPane.setBounds(12, 51, 982, 335);
 		frame.getContentPane().add(layeredPane);
 		
 		JPanel panel = new JPanel();
+		panel.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Vous avez séléctionné la carte : ");
+			}
+		});
+		panel.setBackground(Color.GRAY);
 		panel.setBounds(12, 75, 180, 180);
 		layeredPane.add(panel);
 		panel.setLayout(null);
 		
-		JRadioButton radioButton = new JRadioButton("1");
-		radioButton.setBounds(66, 146, 41, 25);
-		panel.add(radioButton);
-		
 		JPanel panel_1 = new JPanel();
+		panel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "Vous avez séléctionné la carte : ");
+
+			}
+		});
+		panel_1.setBackground(Color.GRAY);
 		panel_1.setBounds(204, 75, 180, 180);
 		layeredPane.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JRadioButton radioButton_1 = new JRadioButton("2");
-		radioButton_1.setBounds(76, 146, 41, 25);
-		panel_1.add(radioButton_1);
-		
 		JPanel panel_2 = new JPanel();
+		panel_2.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Vous avez séléctionné la carte : ");
+			}
+		});
+		panel_2.setBackground(Color.GRAY);
 		panel_2.setBounds(396, 75, 180, 180);
 		layeredPane.add(panel_2);
 		panel_2.setLayout(null);
 		
-		JRadioButton radioButton_2 = new JRadioButton("3");
-		radioButton_2.setBounds(73, 146, 41, 25);
-		panel_2.add(radioButton_2);
-		
 		JPanel panel_3 = new JPanel();
+		panel_3.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Vous avez séléctionné la carte : ");
+			}
+		});
+		panel_3.setBackground(Color.GRAY);
 		panel_3.setBounds(588, 75, 180, 180);
 		layeredPane.add(panel_3);
 		panel_3.setLayout(null);
 		
-		JRadioButton radioButton_3 = new JRadioButton("4");
-		radioButton_3.setBounds(75, 146, 41, 25);
-		panel_3.add(radioButton_3);
-		
 		JPanel panel_4 = new JPanel();
+		panel_4.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Vous avez séléctionné la carte : ");
+			}
+		});
+		panel_4.setBackground(Color.GRAY);
 		panel_4.setBounds(781, 75, 180, 180);
 		layeredPane.add(panel_4);
 		panel_4.setLayout(null);
-		
-		JRadioButton radioButton_4 = new JRadioButton("5");
-		radioButton_4.setBounds(72, 146, 41, 25);
-		panel_4.add(radioButton_4);
-		ButtonGroup bg = new ButtonGroup();
-		bg.add(radioButton);
-		bg.add(radioButton_1);
-		bg.add(radioButton_2);
-		bg.add(radioButton_3);
-		bg.add(radioButton_4);
-		
-		
-		JButton btnNewButton = new JButton("OK");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				JOptionPane.showMessageDialog(null, "Vous avez séléctionné la carte : ","Carte choisie", JOptionPane.INFORMATION_MESSAGE, img);
-			}
-		});
-		btnNewButton.setBounds(395, 282, 97, 25);
-		layeredPane.add(btnNewButton);
+
 		
 	}
 }
